4baeab83796c13a124a59823e404a1e92f87f715
Ira Yakunina <mz@Maxs-Mac-mini.local>
2015-11-02 23:41:36 -0500

    Refactor scene and layer from app.js

--- src/view/scene/MainScene.js
+++ src/view/scene/MainScene.js
@@ -1,3 +1,10 @@
 /**
  * Created by bz on 31/10/2015.
  */
+var MainScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new BackgroundLayer();
+        this.addChild(layer);
+    }
+});
